b4111b9151a144bf2bc469edc686d50fba2afe57
Nadia Tabassum <shormy.992@gmail.com>
2023-01-15 10:58:45 +0600

    Iterarted over all the pages for data

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,16 @@ let descriptionList;
 let registration;
 let totalAdd;
 let adds = [];
+let totalPage = 16;
+const initialUrl = "https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz";
+let pageQueryParams = [];
 
-function getFormulaOneDrivers() {
 
-  return fetch('https://proxy.scrapeops.io/v1/?api_key=b8625ca1-faf5-4841-99c3-db8224b1e995&url=https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz',
+
+function getVehicleData(j) {
+
+
+  return fetch('https://proxy.scrapeops.io/v1/?api_key=b8625ca1-faf5-4841-99c3-db8224b1e995&url=https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz' + pageQueryParams[j],
     {
       headers:
         {"User-Agent": 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36',}
@@ -27,6 +33,9 @@ function getFormulaOneDrivers() {
       const $ = cheerio.load(res, null, false);
       totalAdd = $('.ooa-1000v3p');
       totalAdd = totalAdd[0]?.children[2]?.children[0]?.data?.split(" ")[1].replace(/[{()}]/g, '');
+      //totalPage = $('.pagination-list li:nth-last-child(2) span')?.firstChild?.textContent;
+      //totalPage = parseInt($('.pagination-list li')[5].children[0].children[0].children[0].data) || 16;
+
 
       return $('article').map((i, el) => {
 
@@ -37,24 +46,48 @@ function getFormulaOneDrivers() {
         mileage = descriptionList[0]?.children[2]?.children[0]?.data;
         power = descriptionList[0]?.children[3]?.children[0]?.data;
 
-        adds.push({
+        return {
+          pageNumber: j,
           totalAdd: totalAdd,
-          title:rank[0]?.children[0]?.data,
+          title: rank[0]?.children[0]?.data,
           price: money[0]?.children[0]?.data,
-          registrationYear:registration,
-          mileage:mileage,
+          registrationYear: registration,
+          mileage: mileage,
           power: power,
-        });
+        };
       });
     })
-    .then(() => console.log(adds[0]))
+    .catch((error) => {
+      console.log("========================");
+      console.log("Error Fetching Page ", j);
+      console.log(error);
+      console.log("========================");
+    });
+
+
+}
+
+function getNextPageUrl() {
+  for (let i = 1; i <= totalPage; i++) {
+    pageQueryParams.push("?page=" + i)
+  }
+  //console.log(pageQueryParams);
 }
 
 router.get('/', function (req, res, next) {
-  getFormulaOneDrivers()
-    .then(() => {
-      res.send(adds);
-    });
+  getNextPageUrl();
+  const promises = [];
+  for (i = 1; i <= totalPage; i++) {
+    promises.push(getVehicleData(i));
+
+
+  }
+  Promise.all(promises)
+    .then((adds) => {
+      const responses = [];
+      adds.map(adds => responses.push(...adds))
+      res.send(responses);
+    })
 });
 
 module.exports = router;
